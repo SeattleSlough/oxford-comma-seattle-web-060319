e85b832cf942b3363bfe6d4bd8f59792fd60352d
Michael Maggs <michaelmaggs@gmail.com>
2019-05-14 00:20:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,11 @@ def oxford_comma(array)
     array.each do |element|
       if array.index(element) != array[-1]
         new_array << element
-      else
-        new_array << "and #{element}"
+      
+      end
+   array.each do |element|
+    if array.index(element) == array[-1]  
+      new_array << "and #{element}"
       end
     end
     string = new_array.join(", ")
